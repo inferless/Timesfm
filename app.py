fe19c5d7b627e0e3280b920cb7b1267a10916666
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-05-23 16:30:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+import timesfm
+
+class InferlessPythonModel:
+    def initialize(self):
+        self.tfm = timesfm.TimesFm(
+            context_len=128,
+            horizon_len=96,
+            input_patch_len=32,
+            output_patch_len=128,
+            num_layers=20,
+            model_dims=1280,
+            backend="cpu",
+        )
+        self.tfm.load_from_checkpoint(repo_id="google/timesfm-1.0-200m")
+
+    def infer(self,inputs):
+        forecast_input = inputs["forecast_input"]
+        frequency_input = inputs["frequency_input"]
+        point_forecast, experimental_quantile_forecast = self.tfm.forecast(
+            [forecast_input],
+            freq=[frequency_input],
+        )
+        return {
+            "point_forecast":point_forecast,
+            "experimental_quantile_forecast":experimental_quantile_forecast
+        }
+
+    def finalize(self):
+        pass
 
